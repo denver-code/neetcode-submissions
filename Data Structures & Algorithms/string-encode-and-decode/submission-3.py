6a8c9ff9872fd8da5c9cb9de4fd23d1e82c93a96
Ihor Savenko <csigorek@gmail.com>
2026-07-17 15:17:13 +0100

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        lenghts = ""
+        es = "".join(strs)
+
+        for i, s in enumerate(strs):
+           lenghts += f"{len(s)}{'-' if i != len(strs)-1 else '..'}"
+
+
+        return lenghts+es
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            return []
+        if s == "":
+            return [""]
+        payloads = s.split("..")
+        lenghts = payloads[0].split("-")
+        payload = "".join(payloads[1:])
+        result = []
+        pointer = 0
+        for l in lenghts:
+             l = int(l)
+             word = payload[pointer:pointer+l]
+             result.append(word)
+             pointer += l
+
+        return result
