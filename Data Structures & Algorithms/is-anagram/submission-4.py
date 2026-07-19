bb54ccc9d1679d4e6fa8c6627aaf6547e035011b
Ihor Savenko <csigorek@gmail.com>
2026-07-19 13:18:38 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def get_freq_map(self, s: str) -> dict:
+        freq_map = defaultdict(int)
+
+        for i in s:
+            freq_map[i] += 1
+        
+        return freq_map
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if not s or not t:
+            return False
+        if len(s) != len(t):
+            return False
+        if s == t:
+            return True
+
+        s_f = self.get_freq_map(s)
+        
+        for i in t:
+            if i not in s_f:
+                return False
+            if s_f[i] == 0:
+                return False
+            s_f[i] -= 1
+
+        return True
